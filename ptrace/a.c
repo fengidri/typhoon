29f920183686d683ba4680ac1e307fd389b6ad51
fengidri <fengidri@gmail.com>
2015-09-01 14:24:58 +0800

    add strace

---
+++ ptrace/a.c
@@ -0,0 +1,37 @@
+/**
+ *   author       :   丁雪峰
+ *   time         :   2015-09-01 10:06:57
+ *   email        :   fengidri@yeah.net
+ *   version      :   1.0.1
+ *   description  :
+ */
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <linux/user.h>   /* For constants
+                                   ORIG_EAX etc */
+int main()
+{
+   pid_t child;
+    long orig_eax;
+    child = fork();
+    if(child == 0) {
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        execl("/bin/ls", "ls", NULL);
+    }
+    else {
+        wait(NULL);
+        orig_eax = ptrace(PTRACE_PEEKUSER,
+                          child, 4 * ORIG_EAX,
+                          NULL);
+        printf("The child made a "
+               "system call %ld ", orig_eax);
+        ptrace(PTRACE_CONT, child, NULL, NULL);
+    }
+    return 0;
+}
